853840fc761258d3bf78afde58366eb9845ea3c3
alexhddev <21276888+alexhddev@users.noreply.github.com>
2026-01-07 12:56:07 +0000

    Get By Role - lists

--- Starter/tests/locators/1GetByRole.spec.ts
+++ Starter/tests/locators/1GetByRole.spec.ts
@@ -9,3 +9,18 @@ test('Get by Role practice - heading', async ({ page }) => {
 
     await expect(servicesHeading).toBeVisible()
 })
+
+test('Get by Role - list', async ({ page }) =>{
+    await page.goto('')
+
+    const servicesList = page.getByRole('list')
+    await expect(servicesList).toBeVisible()
+
+    const serviceItems = await servicesList.getByRole('listitem').all()
+
+    for(const item of serviceItems) {
+        const itemText = await item.textContent()
+        expect(itemText).toBeTruthy()
+    }
+
+})
